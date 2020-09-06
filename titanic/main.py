5f79d45fc828190cbd130955bea93680c4c39efb
Mridul Krishna <mridulkrishna1995@gmail.com>
2020-09-06 17:11:23 +0530

    Initial commit

---
+++ titanic/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+
+train_data = pd.read_csv('E:\\Projects\\kaggle\\data\\titanic\\train.csv')
+test_data =  pd.read_csv('E:\\Projects\\kaggle\\data\\titanic\\test.csv')
+
+print(train_data.head())
+print(test_data.head())
